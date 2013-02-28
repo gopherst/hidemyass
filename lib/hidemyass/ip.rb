dce16c488af0a73dfcc46ba48f7993ca14f558bf
Javier Saldana <javier@tractical.com>
2013-02-28 02:30:31 -0600

    comment ip class

--- lib/hidemyass/ip.rb
+++ lib/hidemyass/ip.rb
@@ -1,14 +1,12 @@
-# HideMyAss masks real IPs with CSS classes
-# in order to prevent crawlers from grabbing them.
+# http://hidemyass.com masks the IP addresses
+# with decoy CSS classes and styles so it's hard to read them.
 #
-# Here we emulate what our browser does
-# so we can get the real IP addresses.
-
+# Let's emulate what our browser does in order to get the real address.
 module HideMyAss
   class IP
-    attr_accessor :decoys, :address
 
-    VALID_TAGS = %w(span div text).freeze
+    attr_accessor :decoys, :address
+    TAGS = %w(span div text).freeze
 
     def initialize(encoded_address)
       # Select decoy CSS classes: those which result in a hidden element
@@ -19,7 +17,7 @@ module HideMyAss
       elements = encoded_address.children
 
       # Process elements and get rid of those with decoy classes
-      decode(elements)
+      elements = decode(elements)
 
       # Finally grab enclosing elements content, which is the real IP
       @address = elements.map(&:content).join
@@ -27,15 +25,23 @@ module HideMyAss
     end
 
     # Receives an array of elements
-    # and removes those that are invisible.
+    # and returns another without invisibles.
     def decode(elements)
       elements.each do |element|
-        if !VALID_TAGS.include?(element.name) || (element["style"] && element["style"] =~ /none/)
+
+        # Remove elements with CSS style "none"
+        if !TAGS.include?(element.name) || (element["style"] && element["style"] =~ /none/)
           element.children.remove
+
+        # Get rid of decoy children
         elsif element["class"]
-          decoys.each { |decoy| element.children.remove if decoy.include?(element["class"]) }
+          decoys.each do |decoy|
+            element.children.remove if decoy.include?(element["class"])
+          end
         end
       end
+
+      elements
     end
 
     # Make sure the IP has a valid format.
