14fc26520dfca9f78487bbf17c098fbb3fc771ab
Javier Saldana <javier@tractical.com>
2013-02-28 01:57:30 -0600

    update railtie format

--- lib/hidemyass/railtie.rb
+++ lib/hidemyass/railtie.rb
@@ -2,9 +2,10 @@ require 'hidemyass'
 
 module HideMyAss
   class Railtie < Rails::Railtie
+
     initializer "hidemyass.configure_rails_initialization" do
       HideMyAss.options[:logger] = Rails.logger if defined?(Rails)
       HideMyAss.options[:logger] = ActiveRecord::Base.logger if defined?(ActiveRecord)
     end
-  end
-end
+  end # Railtie
+end # HideMyAss
