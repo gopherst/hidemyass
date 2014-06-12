205c2369988727fa51fff3aff59f55d3175d1585
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2014-06-12 09:27:02 -0700

    Change XPath to account for page structure changes

--- spec/lib/hidemyass/ip_spec.rb
+++ spec/lib/hidemyass/ip_spec.rb
@@ -2,13 +2,13 @@ require "spec_helper"
 
 describe HideMyAss::IP do
 
-  # Tipically we would use webmock here to fake the http request,
+  # Typically we would use webmock here to fake the http request,
   # but hidemyass.com seems to be changing their encoding strategy often.
   # By having real data we ensure this gem is working.
   it "decodes encoded address" do
     html = HideMyAss.send(:get_hma_body)
 
-    HideMyAss::IP.new(html.at_xpath('//table[@id="listtable"]/tr/td[2]/span'))
-      .should be_valid
+    expect(HideMyAss::IP.new(html.at_xpath('//table[@id="listable"]/tbody/tr/td[2]/span')))
+      .to be_valid
   end
 end
